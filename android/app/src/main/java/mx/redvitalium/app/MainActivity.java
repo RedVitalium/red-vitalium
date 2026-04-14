1e41fbca4ab74607588d43f7e9aface6a69666c0
Ruben M <vitalium.mx@gmail.com>
2026-04-15 01:03:36 +0200

    Enable edge-to-edge mode for Android status bar

--- android/app/src/main/java/mx/redvitalium/app/MainActivity.java
+++ android/app/src/main/java/mx/redvitalium/app/MainActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.webkit.WebView;
 import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.view.WindowCompat;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
 
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
