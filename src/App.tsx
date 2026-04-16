c3728eb74740bd9f557df8dfb5b90edce0113d7a
Ruben M <vitalium.mx@gmail.com>
2026-04-16 21:52:40 +0200

    Reduce keyboard scroll delay to 50ms

--- src/App.tsx
+++ src/App.tsx
@@ -148,7 +148,7 @@ function KeyboardHandler() {
       setTimeout(() => {
         const el = document.activeElement as HTMLElement;
         if (el) el.scrollIntoView({ behavior: 'smooth', block: 'center' });
-      }, 150);
+      }, 50);
     });
 
     const hideListener = Keyboard.addListener('keyboardWillHide', () => {
